3bb7c85610a305dcb60a47fd7b215ae615e7c127
Cole Sturza <cost5824@colorado.edu>
2018-10-13 16:51:59 -0600

    Add SchoolMajor db class

---
+++ Website/UHub.CoreLib/Entities/SchoolMajors/SchoolMajor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UHub.CoreLib.Attributes;
+
+namespace UHub.CoreLib.Entities.SchoolMajors
+{
+    [DataClass]
+    public sealed partial class SchoolMajor
+    {
+        [DataProperty(EnableDBColumnValidation: false)]
+        public long? ID { get; set; }
+
+        [DataProperty(EnableDBColumnValidation: false)]
+        public string Name { get; set; }
+
+        [DataProperty(EnableDBColumnValidation: false)]
+        public bool IsEnabled { get; set; }
+
+        [DataProperty(EnableDBColumnValidation: false)]
+        public bool IsReadOnly { get; set; }
+
+        [DataProperty(EnableDBColumnValidation: false)]
+        public string Description { get; set; }
+
+        [DataProperty(EnableDBColumnValidation: false)]
+        public bool IsDeleted { get; set; }
+
+        [DataProperty(EnableDBColumnValidation: false)]
+        public bool CreatedBy { get; set; }
+
+        [DataProperty(EnableDBColumnValidation: false)]
+        public DateTimeOffset CreatedDate { get; set; }
+
+        [DataProperty(EnableDBColumnValidation: false)]
+        public bool ModifiedBy { get; set; }
+
+        [DataProperty(EnableDBColumnValidation: false)]
+        public DateTimeOffset ModifiedDate { get; set; }
+    }
+}
