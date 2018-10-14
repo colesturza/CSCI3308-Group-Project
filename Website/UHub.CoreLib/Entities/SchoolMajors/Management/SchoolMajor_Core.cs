48803a583eb430734673b7268b9f66b1c1d1502e
Cole Sturza <cost5824@colorado.edu>
2018-10-13 21:49:23 -0600

    Implemented the SchoolMajorReader class, and made the SchoolMajor class inherit from DBEntityBase.

---
+++ Website/UHub.CoreLib/Entities/SchoolMajors/Management/SchoolMajor_Core.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UHub.CoreLib.DataInterop;
+using UHub.CoreLib.ErrorHandling.Exceptions;
+using UHub.CoreLib.Extensions;
+using UHub.CoreLib.Management;
+
+namespace UHub.CoreLib.Entities.SchoolMajors.Management
+{
+    public static partial class SchoolMajorReader
+    {
+        private static string _dbConn = null;
+
+        static SchoolMajorReader()
+        {
+            _dbConn = CoreFactory.Singleton.Properties.CmsDBConfig;
+        }
+
+        #region Individual
+        /// <summary>
+        /// Get DB school major full detail by GUID UID
+        /// </summary>
+        /// <param name="SchoolMajorID"></param>
+        /// <returns></returns>
+        public static SchoolMajor GetSchoolMajor(long SchoolMajorID)
+        {
+            if (!CoreFactory.Singleton.IsEnabled)
+            {
+                throw new SystemDisabledException();
+            }
+
+
+            return SqlWorker.ExecBasicQuery(
+                _dbConn,
+                "[dbo].[SchoolMajor_GetByID]",
+                (cmd) =>
+                {
+                    cmd.Parameters.Add("@SchoolMajorID", SqlDbType.BigInt).Value = SchoolMajorID;
+                },
+                (reader) =>
+                {
+                    return reader.ToCustomDBType<SchoolMajor>();
+                }).SingleOrDefault();
+        }
+        #endregion Individual
+
+        #region Group
+        /// <summary>
+        /// Get all the school majors in the DB
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<SchoolMajor> GetSchoolMajors()
+        {
+
+            if (!CoreFactory.Singleton.IsEnabled)
+            {
+                throw new SystemDisabledException();
+            }
+
+
+            return SqlWorker.ExecBasicQuery(
+                _dbConn,
+                "[dbo].[SchoolMajor_GetAll]",
+                (cmd) => { },
+                (row) =>
+                {
+                    return row.ToCustomDBType<SchoolMajor>();
+                });
+        }
+
+        /// <summary>
+        /// Get all the school majors in the DB
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<SchoolMajor> GetSchoolMajorsBySchool(long SchoolID)
+        {
+
+            if (!CoreFactory.Singleton.IsEnabled)
+            {
+                throw new SystemDisabledException();
+            }
+
+
+            return SqlWorker.ExecBasicQuery(
+                _dbConn,
+                "[dbo].[SchoolMajor_GetBySchool]",
+                (cmd) => {
+                    cmd.Parameters.Add("@SchoolID", SqlDbType.BigInt).Value = SchoolID;
+                },
+                (row) =>
+                {
+                    return row.ToCustomDBType<SchoolMajor>();
+                });
+        }
+        #endregion Group
+    }
+}
--- Website/UHub.CoreLib/Entities/SchoolMajors/SchoolMajor.cs
+++ Website/UHub.CoreLib/Entities/SchoolMajors/SchoolMajor.cs
@@ -4,11 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UHub.CoreLib.Attributes;
+using UHub.CoreLib.DataInterop;
 
 namespace UHub.CoreLib.Entities.SchoolMajors
 {
     [DataClass]
-    public sealed partial class SchoolMajor
+    public sealed partial class SchoolMajor : DBEntityBase
     {
         [DataProperty(EnableDBColumnValidation: false)]
         public long? ID { get; set; }
