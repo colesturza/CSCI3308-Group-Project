934c57d43dce3e6bdc29079e0cff2712267e228f
Cole Sturza <cost5824@colorado.edu>
2018-10-27 15:27:55 -0600

    Add club moderator controller and writer

---
+++ Website/UHub.CoreLib/Entities/ClubModerators/DTOs/ClubModerator_C_PublicDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UHub.CoreLib.Attributes;
+using UHub.CoreLib.DataInterop;
+using UHub.CoreLib.Entities.ClubModerators.Interfaces;
+
+namespace UHub.CoreLib.Entities.ClubModerators.DTOs
+{
+    [DtoClass(typeof(ClubModerator))]
+    public sealed partial class ClubModerator_C_PublicDTO : DtoEntityBase, IClubModerator_C_Public
+    {
+        public long UserID { get; set; }
+        public bool IsOwner { get; set; }
+        public bool IsEnabled { get; set; }
+        public bool IsReadOnly { get; set; }
+    }
+}
