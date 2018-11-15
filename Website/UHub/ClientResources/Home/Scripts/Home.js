7acf6d1bfade6c85a3860d6700f28c4e9adb3d89
Cole Sturza <cost5824@colorado.edu>
2018-11-15 03:31:01 -0700

    Added functionality to route users to the community pages from the community dropdown.

--- Website/UHub/ClientResources/Home/Scripts/Home.js
+++ Website/UHub/ClientResources/Home/Scripts/Home.js
@@ -10,16 +10,6 @@ var homePosts = $.ajax({
     headers: {auth: ""}
 });
 
-var examplePosts = [{ postid: 1, url:'#',subject: "LoremIpsum", postContent: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eros mauris, mollis at fermentum mattis, interdum et ipsum." },
-    {postid: 2, url:'#',subject: "IpsumLorem", postContent: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eros mauris, mollis at fermentum mattis, interdum et ipsum."},
-    {postid: 3, url:'#',subject: "LoremIpsum", postContent: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eros mauris, mollis at fermentum mattis, interdum et ipsum."},
-    {postid: 4, url:'#',subject: "IpsumLorem", postContent: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eros mauris, mollis at fermentum mattis, interdum et ipsum."},
-    {postid: 4, url:'#',subject: "IpsumLorem", postContent: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eros mauris, mollis at fermentum mattis, interdum et ipsum."},
-    {postid: 4, url:'#',subject: "IpsumLorem", postContent: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eros mauris, mollis at fermentum mattis, interdum et ipsum."},
-    {postid: 4, url:'#',subject: "IpsumLorem", postContent: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eros mauris, mollis at fermentum mattis, interdum et ipsum."}];
-
-var exampleCommunities = [{name: "Anthropology",url: "www.google.com"},{name: "Computer Science B.S.", url: "#"}, {name: "Mechanical Engineering", url: "#"}];
-
 var communityDropdown = new Vue({
     el: "#communityDrop",
     data: {
@@ -29,6 +19,9 @@ var communityDropdown = new Vue({
         getCommunities(){
             this.communities = communityRequest;
             console.log(this.communities);
+        },
+        goToClubPage(id) {
+            window.location.href = "http://" + window.location.hostname + "/SchoolClub/" + id;
         }
     },
     beforeMount(){
