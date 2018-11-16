bd5cffc8c1666690f35951b48d769e688d93f8fc
Cole Sturza <cost5824@colorado.edu>
2018-11-15 20:22:58 -0700

    testing

--- Website/UHub/ClientResources/Home/Scripts/Home.js
+++ Website/UHub/ClientResources/Home/Scripts/Home.js
@@ -1,63 +1,36 @@
-var communityRequest;
-$.ajax({
-    method: "POST",
-    url: "/uhubapi/schoolclubs/GetAllBySchool",
-    success: function (data) {
-        console.log(data);
-        communityRequest = data;
-        console.log(communityRequest);
-    },
-    statusCode: {
-        401: function () {
-            communityRequest = null;
-        }
-    }
-});
-
-var homePosts;
-$.ajax({
-    method: "POST",
-    url: "/uhubapi/posts/GetAllBySchool",
-    success: function (data) {
-        console.log(data);
-        homePosts = data;
-        console.log(homePosts);
-    },
-    statusCode: {
-        401: function () {
-            homePosts = null;
-        }
-    }
-});
-
 var communityDropdown = new Vue({
     el: "#communityDrop",
-    data: {
-        communities: []
-    },
-    methods: {
-        getCommunities(){
-            this.communities = communityRequest;
-            console.log(this.communities);
+    data() {
+        var communityRequest = $.ajax({
+            method: "POST",
+            url: "/uhubapi/schoolclubs/GetAllBySchool",
+            statusCode: {
+                401: function () {
+                    communityRequest = [];
+                }
+            }
+        });
+
+        return {
+            communities: communityRequest
         }
-    },
-    beforeMount(){
-        this.getCommunities()
     }
 });
 
 var postList = new Vue({
     el: "#post-list",
-    data: {
-        posts: []
-    },
-    methods: {
-        getPosts(){
-            this.posts = homePosts;
-            console.log(this.posts);
+    data() {
+        var homePosts = $.ajax({
+            method: "POST",
+            url: "/uhubapi/posts/GetAllBySchool",
+            statusCode: {
+                401: function () {
+                    homePosts = [];
+                }
+            }
+        });
+        return {
+            posts: homePosts
         }
-    },
-    beforeMount(){
-        this.getPosts()
     }
 });
