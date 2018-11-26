10d26f8913ac873544a5672fb756f237ea35d182
Austin <aual1780@colorado.edu>
2018-11-26 16:29:40 -0700

    Improve HTML optimization

--- Website/UHub/App_Start/BundleConfig.cs
+++ Website/UHub/App_Start/BundleConfig.cs
@@ -14,27 +14,58 @@ namespace UHub
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery")
+                .Include("~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval")
+                .Include("~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            bundles.Add(new ScriptBundle("~/bundles/modernizr")
+                .Include("~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap")
+                .Include("~/Scripts/bootstrap.js"));
 
 
+
+            //---------------------------------MASTER------------------------------------------------------
             bundles.Add(new ScriptBundle("~/resources/scripts/master")
                 .IncludeDirectory("~/ClientResources/Master/Scripts", "*.js", true));
 
 
+
+
+            //---------------------------------ACCOUNT------------------------------------------------------
+            //LOGIN
+            bundles.Add(
+                new ScriptBundle("~/resources/account/loginScript")
+                .IncludeDirectory("~/ClientResources/Master/Scripts", "*.js", true)
+                .Include("~/ClientResources/Account/Scripts/Login.js")
+                .Include("~/ClientResources/Security/Scripts/RecaptchaPartial.js")
+                .Include("~/Scripts/jquery.validate*")
+                );
+
+
+            //CREATE
+            bundles.Add(
+                new ScriptBundle("~/resources/account/createScript")
+                .IncludeDirectory("~/ClientResources/Master/Scripts", "*.js", true)
+                .Include("~/ClientResources/Account/Scripts/CreateAccount.js")
+                .Include("~/ClientResources/Security/Scripts/RecaptchaPartial.js")
+                );
+            bundles.Add(
+                new CustomStyleBundle("~/resources/account/createStyle")
+                .IncludeDirectory("~/ClientResources/Master/Styles", "*css", true)
+                .Include("~/ClientResources/Account/Styles/CreateAccount.css")
+                );
+
+
+
             bundles.Add(new CustomStyleBundle("~/resources/css/master")
                 .IncludeDirectory("~/ClientResources/Master/Styles", "*css", true));
+
 
 
             BundleTable.EnableOptimizations = true;
