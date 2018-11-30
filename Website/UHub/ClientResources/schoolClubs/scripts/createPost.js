bac6558b7505c6b35d7214ba852f257e15580305
aual1780 <aual1780@colorado.edu>
2018-11-29 17:18:55 -0700

    Update create post JS

--- Website/UHub/ClientResources/schoolClubs/scripts/createPost.js
+++ Website/UHub/ClientResources/schoolClubs/scripts/createPost.js
@@ -7,23 +7,23 @@ console.log(commentID)
 new Vue({
   el: '#enterInfo',
   data: {
-    title: "",
-    content: "",
-    public: "",
-    comment: "",
-    parentID: ""
+    Name: "",
+    Content: "",
+    IsPublic: "",
+    CanComment: "",
+    ParentID: ""
   },
   methods: {
       readRefs: function () {
       let refs = this.$refs;
       
-      this.title = refs.title.value
-      this.content = refs.content.value
-      this.public = refs.public.checked
-      this.comment = refs.comment.checked
-      this.parentID = commentID
-      this.postobj = JSON.stringify(this.$data)
-      console.log(JSON.stringify(this.$data))
+      this.Name = refs.inputTitle.value;
+      this.Content = refs.inputContent.value;
+      this.IsPublic = !refs.inputMakePrivate.checked;
+      this.CanComment = refs.inputCanComment.checked;
+      this.ParentID = commentID;
+      this.postobj = JSON.stringify(this.$data);
+      console.log(JSON.stringify(this.$data));
     }
   },
   watch: {
