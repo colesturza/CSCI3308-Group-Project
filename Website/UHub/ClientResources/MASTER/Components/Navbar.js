53462b1a87cf238eb8941a90e57c5e8115bb3d14
Austin <aual1780@colorado.edu>
2018-11-30 03:46:31 -0700

    remove deprecated Navbar element

--- Website/UHub/ClientResources/MASTER/Components/Navbar.js
+++ Website/UHub/ClientResources/MASTER/Components/Navbar.js
@@ -73,25 +73,6 @@
                     
                 </nav>
             </div>
-
-            <div id="auxNav" class="container-fullwidth">
-                <ul class="nav navbar-expand-md bg-light justify-content-center">
-                    <li class="nav-item navbar-brand pl-3 pr-0 pt-1">
-                        <span class="align-middle">Sort</span>
-                    </li>
-                    <li class="nav-item px-3 py-2">
-                        <div class="dropdown">
-                            <button class="btn btn-sm dropdown-toggle" type="button" id="sortDropdownButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Select
-                            </button>
-                            <div class="dropdown-menu" aria-labelledby="sortDropdownButton">
-                                <a class="dropdown-item" href="#">Popular</a>
-                                <a class="dropdown-item" href="#">Date Posted</a>
-                            </div>
-                        </div>
-                    </li>
-                </ul>
-            </div>
         </div>
         `
     });
