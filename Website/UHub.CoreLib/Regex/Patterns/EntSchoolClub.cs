bebf46eb36506807ec637c076eb7caf052a353bb
Austin <aual1780@colorado.edu>
2018-12-04 18:18:09 -0700

    Update club name filter

--- Website/UHub.CoreLib/Regex/Patterns/EntSchoolClub.cs
+++ Website/UHub.CoreLib/Regex/Patterns/EntSchoolClub.cs
@@ -9,8 +9,8 @@ namespace UHub.CoreLib.Regex.Patterns
     public static class EntSchoolClub
     {
 
-        public const string NAME = @"(([ \u00c0-\u01ffA-z'\-])+){3,100}";
-        public const string NAME_B = @"^(([ \u00c0-\u01ffA-z'\-])+){3,100}$";
+        public const string NAME = @"(([ \u00c0-\u01ffA-z0-9'\-])+){3,100}";
+        public const string NAME_B = @"^(([ \u00c0-\u01ffA-z0-9'\-])+){3,100}$";
 
         public const string DESCRIPTION = @".{1,2000}";
         public const string DESCRIPTION_B = @"^.{1,2000}$";
