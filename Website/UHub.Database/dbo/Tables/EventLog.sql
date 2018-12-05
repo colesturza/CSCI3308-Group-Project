01e95b91df873947a7836ca3b193049ebdf042ff
Austin <aual1780@colorado.edu>
2018-12-04 18:06:40 -0700

    Add DB event changes

---
+++ Website/UHub.Database/dbo/Tables/EventLog.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[EventLog] (
+    [EventTypeID] SMALLINT           NOT NULL,
+    [EventID]     NVARCHAR (100)     NULL,
+    [Content]     NVARCHAR (MAX)     NOT NULL,
+    [CreatedBy]   BIGINT             CONSTRAINT [DF_EventLog_CreatedBy] DEFAULT ((0)) NOT NULL,
+    [CreatedDate] DATETIMEOFFSET (7) CONSTRAINT [DF_EventLog_CreatedDate] DEFAULT (sysdatetimeoffset()) NOT NULL,
+    CONSTRAINT [FK_EventLog_EventLogTypes] FOREIGN KEY ([EventTypeID]) REFERENCES [dbo].[EventLogTypes] ([ID])
+);
+
+
+GO
+CREATE CLUSTERED INDEX [IX_EventLog]
+    ON [dbo].[EventLog]([EventID] ASC);
+
