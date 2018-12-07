33908b21b20cb7aca083141607c65a75f326d366
aual1780 <aual1780@colorado.edu>
2018-12-06 17:48:36 -0700

    fix post JS

--- Website/UHub/ClientResources/Post/Scripts/PostUpdatable.js
+++ Website/UHub/ClientResources/Post/Scripts/PostUpdatable.js
@@ -150,7 +150,7 @@
             '                    </div>' +
             '                </div>' +
             '               <div>' +
-            '                   <comment-component v-for="comment in comment.cmt_children.sort(dynamicSort("-CreatedDate"))" v-on:custom-click="buttonHandler" :key="comment.id" v-bind:comment="comment"><comment-component>' +
+            '                   <comment-component v-for="comment in comment.cmt_children.sort(dynamicSort(\'-CreatedDate\'))" v-on:custom-click="buttonHandler" :key="comment.id" v-bind:comment="comment"><comment-component>' +
             '               </div>' +
             '            </div>' +
             '        </div>',
