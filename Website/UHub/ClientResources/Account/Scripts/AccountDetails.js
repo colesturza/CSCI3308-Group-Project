aa00479b60b982844a1ca18c97f3d57e2134975f
Nicholas Volpe <nivo2681@colorado.edu>
2018-12-06 18:33:27 -0700

    Added ajax call to Find

--- Website/UHub/ClientResources/Account/Scripts/AccountDetails.js
+++ Website/UHub/ClientResources/Account/Scripts/AccountDetails.js
@@ -1,15 +1,24 @@
-﻿
-getUser = new Vue({
-	el: "#user-info",
-	data: {
-		user: {}
-	},
-	methods: {
-		user_get() {
-			this.user = user
-		},
-	},
-	beforeMount() {
-		this.user_get()
-	}
-});
+﻿(function(){
+	var userID = window.location.href.split('/').slice(-1)[0];
+    var getUser = new Vue({
+        el: "#user-info",
+        data: {
+            user: {}
+        },
+        methods: {
+            user_get() {
+                $.ajax({
+                    method: "POST",
+                    url: "/uhubapi/users/GetByID?UserID=" + userID,
+                })
+					.done(function(data) {
+                    console.log(data);
+                    this.user = data;
+                });
+            }
+        },
+        beforeMount() {
+            this.user_get()
+        }
+    });
+})();
