54feda7c711c083f2bb06949c8a142169077bc3e
Cole Sturza <cost5824@colorado.edu>
2018-12-08 16:45:50 -0700

    Fixed bug.

--- Website/UHub/ClientResources/Post/Scripts/Post.js
+++ Website/UHub/ClientResources/Post/Scripts/Post.js
@@ -160,7 +160,7 @@
             '            <div class="container-fluid">' +
             '                <div>' +
             '                    <span class="m-2 mr-0" style="margin-right:0 !important">Posted by</span>' +
-            '                    [<a v-bind:href="'/Account/find/' + comment.CreatedBy">{{ comment.Username }}</a>]' +
+            '                    [<a v-bind:href="\'/Account/find/\' + comment.CreatedBy">{{ comment.Username }}</a>]' +
             '                    <span> • {{ comment.CreatedDate.toString().substring(0,10) }}</span>' +
             '                </div>' +
             '                <div class="border border-dark rounded m-2 py-2">' +
