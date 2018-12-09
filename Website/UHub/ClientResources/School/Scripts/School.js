0f1212d99611c4ae282861d6b474eaa6f1b0a9c9
Austin <aual1780@colorado.edu>
2018-12-08 22:31:19 -0700

    remove loop

--- Website/UHub/ClientResources/School/Scripts/School.js
+++ Website/UHub/ClientResources/School/Scripts/School.js
@@ -28,16 +28,15 @@
 
                     if (data.length > 0) {
 
-                        for (var i = 0; i < data.length; i++) {
-                            data[i].Content = mdConverter.makeHtml(data[i].Content);
-                            data[i].dateCreatedFromNow = moment(data[i].CreatedDate).fromNow();
-                        }
                         data.sort(dynamicSort("-CreatedDate"));
-
 
                         //set parentName
                         var clubSet = $("#navbarDropdownMenu a.dropdown-item");
                         for (var i = 0; i < data.length; i++) {
+                            data[i].Content = mdConverter.makeHtml(data[i].Content);
+                            data[i].dateCreatedFromNow = moment(data[i].CreatedDate).fromNow();
+
+
                             for (var j = 0; j < clubSet.length; j++) {
                                 if (data[i].ParentID == $(clubSet[j]).attr("data-clubID")) {
                                     data[i].ClubName = $(clubSet[j]).text();
