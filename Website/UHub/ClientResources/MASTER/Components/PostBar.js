b96ece82d8034b1a991e1f5a1013cd4be3509dfb
Austin <aual1780@colorado.edu>
2018-12-08 20:43:54 -0700

    UI updates

--- Website/UHub/ClientResources/MASTER/Components/PostBar.js
+++ Website/UHub/ClientResources/MASTER/Components/PostBar.js
@@ -25,27 +25,11 @@
                         <a :href="createURL" id="btn_HeaderCreatePost" class="btn btn-secondary acctLnk" style="padding:3px;margin:5px 0 0 0">Create Post</a>
                     </div>
                 </li>
-                <li class="w-15">
-                </li>
-                <li class="nav-item navbar-brand pl-3 pr-0 pt-1">
-                    <span class="align-middle">Sort</span>
-                </li>
-                <li class="nav-item px-3 py-2 col-sm-1">
-                    <div class="dropdown">
-                        <button class="btn btn-sm dropdown-toggle" type="button" id="sortDropdownButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Select
-                        </button>
-                        <div class="dropdown-menu" aria-labelledby="sortDropdownButton">
-                            <a class="dropdown-item" href="#">Popular</a>
-                            <a class="dropdown-item" href="#">Date Posted</a>
-                        </div>
-                    </div>
-                </li>
             </ul>
         </div>
         `
     });
 
-    new Vue({ el: "#postbar-uhub" })
+    new Vue({ el: "#postbar-uhub" });
 
 })();
